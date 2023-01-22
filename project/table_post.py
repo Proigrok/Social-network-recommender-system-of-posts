513330695c547ef8d0874df41ee9314b78d0c747
Вячеслав Сусанин <v-susanin@yandex.ru>
2023-01-22 19:45:22 +0300

    2 урок(сдано)

--- project/table_post.py
+++ project/table_post.py
@@ -1,4 +1,4 @@
-from database import Base
+from database import Base, SessionLocal
 from sqlalchemy import Column, Integer, String
 
 
@@ -11,4 +11,13 @@ class Post(Base):
     id = Column(Integer, primary_key=True)
     text = Column(String)
     topic = Column(String)
+
+if __name__ == "__main__":
+    session = SessionLocal()
+    k = session.query(Post.id).filter(Post.topic == "business").order_by(Post.id.desc()).limit(10).all()
+
+    print([id[0] for id in k])
+
+
+
 
