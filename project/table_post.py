a09d914cfb29f646de24a96554b2be81b4471b90
Вячеслав Сусанин <v-susanin@yandex.ru>
2023-01-22 18:17:46 +0300

    1 урок(сдано)

--- project/table_post.py
+++ project/table_post.py
@@ -1,4 +1,10 @@
-from project.database import Base
+from database import Base
+from sqlalchemy import Column, Integer, String
+
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# NOTE be careful when sharing
+SQLALCHEMY_DATABASE_URL = "postgresql://username:password@localhost/database"
 
 class Post(Base):
     __tablename__ = "post"
