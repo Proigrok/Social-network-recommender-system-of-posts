58fdcef1e80ee06ff1b5f40f9b51b037e49b6ccf
Вячеслав Сусанин <v-susanin@yandex.ru>
2023-01-23 17:39:35 +0300

    5 урок(сдано)

---
+++ project/table_feed.py
@@ -0,0 +1,20 @@
+from database import Base, SessionLocal
+from sqlalchemy import Column, Integer, String,TIMESTAMP, func, ForeignKey
+from table_post import Post
+from table_user import User
+
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# NOTE be careful when sharing
+SQLALCHEMY_DATABASE_URL = "postgresql://username:password@localhost/database"
+
+class Feed(Base):
+    __tablename__ = "feed_action"
+    user_id = Column(Integer, ForeignKey(User.id), primary_key=True, index=True)
+    post_id = Column(Integer, ForeignKey(User.id), primary_key=True, index=True)
+    action = Column(String)
+    time = Column(TIMESTAMP)
+
+
+
+
